59f7f0f4a42b9cae1df81be7e7d78f9ef326d6f8
BuildTools <unconfigured@null.spigotmc.org>
2023-08-15 14:39:37 +0900

    fix error

--- src/main/java/com/jnjnetwork/CodeBank/config/PrincipalDetails.java
+++ src/main/java/com/jnjnetwork/CodeBank/config/PrincipalDetails.java
@@ -62,7 +62,7 @@ public class PrincipalDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return user.getEmail();
     }
 
     @Override
