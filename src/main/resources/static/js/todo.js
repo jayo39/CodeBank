55ab32331b5b7be8ebcb60c4af5ee86dc615e523
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 17:08:33 +0900

    add sidebar, fix color animation

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -1,13 +1,26 @@
 $(function() {
+    const sidebar = $('#sidebar');
+    const openSidebar = $('#todo_alert');
+    const closeSidebar = $('#closeSidebar')
+
     $('#todo_alert').css({'color': "#ffffff"});
 
-    $('#todo_alert').animate({'color': "#ff8c00"}, 1050).promise()
+    $('#todo_alert').animate({'color': "#FF8C00"}, 1050).promise()
         .then(function() {
             loopAlert();
         });
 
     function loopAlert() {
         $('#todo_alert').animate({'color': "#ffffff"}, 1050);
-        $('#todo_alert').animate({'color': "#ff8c00"}, 1050, loopAlert);
+        $('#todo_alert').animate({'color': "#FF8C00"}, 1050, loopAlert);
     }
+
+    openSidebar.click(function() {
+      sidebar.css('right', '0');
+    });
+
+    closeSidebar.click(function() {
+      sidebar.css('right', '-350px');
+    });
+
 });
