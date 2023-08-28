5752f4448fa004881feb292775c53752ddfefd56
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 16:19:13 +0900

    add todo color animation

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -1,3 +1,13 @@
 $(function() {
+    $('#todo_alert').css({'color': "#ffffff"});
 
+    $('#todo_alert').animate({'color': "#ff8c00"}, 1050).promise()
+        .then(function() {
+            loopAlert();
+        });
+
+    function loopAlert() {
+        $('#todo_alert').animate({'color': "#ffffff"}, 1050);
+        $('#todo_alert').animate({'color': "#ff8c00"}, 1050, loopAlert);
+    }
 });
